4c3c458097e5fd1e991168a8aa01f9a161b9bcc1
nneishant <nishantphatak181199@gmail.com>
2024-10-31 04:06:34 -0400

    first commit

---
+++ order_history.php
@@ -0,0 +1,75 @@
+<?php
+include ("header.php");
+if(!isset($_SESSION['FOOD_USER_ID'])){
+	redirect(FRONT_SITE_PATH.'shop');
+}
+$uid=$_SESSION['FOOD_USER_ID'];
+$sql="select order_master.*,order_status.order_status as order_status_str from order_master,order_status where order_master.order_status=order_status.id and order_master.user_id='$uid' order by order_master.id desc";
+
+$res=mysqli_query($con,$sql);
+?>
+
+<div class="cart-main-area pt-95 pb-100">
+            <div class="container">
+                <h3 class="page-title">Order History</h3>
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-12">
+                        <form method="post">
+							<div class="table-content table-responsive">
+								
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th>Order No</th>
+                                            <th>Price</th>
+											<th>Coupon</th>
+                                            <th>Address</th>
+											<th>Zipcode</th>
+                                            <th>Order Status</th>
+                                            <th>Payment Status</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+										<?php if(mysqli_num_rows($res)>0){
+										$i=1;
+										while($row=mysqli_fetch_assoc($res)){
+										?>
+										<tr>
+                                            <td>
+												<div class="div_order_id"><a href="<?php echo FRONT_SITE_PATH.'order_detail?id='.$row['id']?>"><?php echo $row['id']?></div></a>
+											
+											<br/>
+											<a href="<?php echo FRONT_SITE_PATH?>download_invoice?id=<?php echo $row['id']?>"><img src='<?php echo FRONT_SITE_PATH?>assets/img/icon-img/pdf.png' width="20px" title="Download Invoice"/></a>
+											</td>
+                                            <td style="font-size:14px;">
+											<?php echo $row['total_price']?></td>
+                                            <td>
+											<?php
+											if($row['coupon_code']!=''){
+											?>
+											Coupon Code:- <?php echo $row['coupon_code']?><br/>
+											Final Price:- <?php echo $row['final_price']?>
+											<?php } ?>
+											</td>
+											<td><?php echo $row['address']?></td>
+											<td><?php echo $row['zipcode']?></td>
+											<td><?php echo $row['order_status_str']?></td>
+											<td>
+												<div class="payment_status payment_status_<?php echo $row['payment_status']?>"><?php echo ucfirst($row['payment_status'])?></div>
+											</td>
+                                        </tr>
+										<?php }} ?>
+                                    </tbody>
+                                </table>
+								
+                            </div>
+                            
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+<?php
+include("footer.php");
+?>
